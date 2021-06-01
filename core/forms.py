f3919abfaa43d08bc9935ecd58cc7542f04e5bbc
Paul-Ngigi <paulkush7777@gmail.com>
2021-06-01 22:29:05 +0300

    add posts form

--- core/forms.py
+++ core/forms.py
@@ -7,3 +7,10 @@ class ProfileForm(forms.ModelForm):
         model = Profile
         exclude = ['user']
         fields = ['dp', 'bio', 'phone_number']
+
+
+class PostsForm(forms.ModelForm):
+    class Meta:
+        model = Posts
+        exclude = ['user']
+        fields = ['name', 'description', 'link', 'image1', 'image2', 'image3']
