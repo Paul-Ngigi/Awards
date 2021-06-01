770708b03334a9c9d2012a91eb68c4cb0f4ce06f
Paul-Ngigi <paulkush7777@gmail.com>
2021-06-01 22:30:20 +0300

    add index view

--- core/views.py
+++ core/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import View
+from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
+from .models import Posts, Likes
+from .forms import PostsForm, CommentsForm, VotesForm, ProfileForm
+
+User = get_user_model()
+
 
 # Create your views here.
+class IndexView(View):
+    """
+    Index view
+    """
+
+    def get(self, request, *args, **kwargs):
+        form = PostsForm()
+        posts = Posts.objects.all()
+        return render(request, "core/index.html", {"posts": posts, "form": form})
+
+    def post(self, request, *args, **kwargs):
+        form = PostsForm(request.POST, request.FILES)
+        posts = Posts.objects.all()
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+            return render(request, "core/index.html", {"posts": posts, "form": form})
+        else:
+            return render(request, "core/index.html", {"posts": posts, "form": form})
