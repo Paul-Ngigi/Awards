32db8112e7494c71d07d7e665bd5b1fa6d5455e9
Paul-Ngigi <paulkush7777@gmail.com>
2021-06-01 22:13:04 +0300

    add urls

---
+++ authentication/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from .views import SignUp, Login, SignOut
+from django_registration.backends.one_step.views import RegistrationView
+
+urlpatterns = [
+    path('tinymce/', include('tinymce.urls')),
+    path('login', Login.as_view(), name='login_view'),
+    path('signup/', SignUp.as_view(), name='signup_view'),
+    path('signout/', SignOut.as_view(), name='signout_view'),
+    path('auth/', include('django_registration.backends.one_step.urls')),
+    path('auth/', include('django.contrib.auth.urls')),
+    path('accounts/register/',
+         RegistrationView.as_view(success_url='/profile/'),
+         name='django_registration_register'),
+]
