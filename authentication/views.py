11957e5e3634c5cf4db59398347750dff43dcdf2
Paul-Ngigi <paulkush7777@gmail.com>
2021-06-01 22:11:27 +0300

    add login view

--- authentication/views.py
+++ authentication/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import get_user_model, authenticate, login, logout
 from django.views.generic import View
 from .forms import LoginForm, RegistrationForm
-from core.models import Profile
 
 User = get_user_model()
 
@@ -42,5 +41,47 @@ class SignUp(View):
             else:
                 request.session['register_error'] = 1
 
+        context = {"form": form}
+        return render(request, self.template_name, context)
+
+
+class Login(View):
+    """
+    Sign in view
+    """
+    form = LoginForm()
+    template_name = 'authentication/signin.html'
+
+    def get(self, request, *args, **kwargs):
+        print("anything1")
+        if request.user.is_authenticated:
+            print("anything2")
+            return redirect('home_view')
+        return render(request, self.template_name)
+
+    def post(self, request, *args, **kwargs):
+        form = LoginForm(request.POST)
+        print("anything3")
+        if form.is_valid():
+            print("anything4")
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            print(username)
+            print(password)
+            if username and password:
+                print("anything5")
+                user = authenticate(request, username=username, password=password)
+                print("anything6")
+                if user is not None:
+                    print("anything7")
+                    login(request, user)
+                    print("anything8")
+                    return redirect('home_view')
+
+                else:
+                    request.session['invalid_user'] = 1
+
+        else:
+            print("Error")
         context = {"form": form}
         return render(request, self.template_name, context)
