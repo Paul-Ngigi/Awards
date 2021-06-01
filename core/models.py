53b35f9a811a027f1979fd0fe9ba8aa40d74995a
Paul-Ngigi <paulkush7777@gmail.com>
2021-06-01 22:24:33 +0300

    add post model

--- core/models.py
+++ core/models.py
@@ -23,3 +23,43 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+class Posts(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=5000, null=True)
+    link = models.URLField()
+    image1 = ImageField(manual_crop='1024x1024')
+    image2 = ImageField(manual_crop='1024x1024')
+    image3 = ImageField(manual_crop='1024x1024')
+    postedon = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        ordering = ['-id']
+
+    @classmethod
+    def save_post(self):
+        self.save()
+
+    @classmethod
+    def get_posts(cls):
+        posts = cls.objects.all()
+        return posts
+
+    @classmethod
+    def delete_post(self):
+        self.delete()
+
+    @classmethod
+    def delete_image_by_id(cls, id):
+        post = cls.objects.filter(pk=id)
+        post.delete()
+
+    @classmethod
+    def get_image_by_id(cls, id):
+        post = cls.objects.get(pk=id)
+        return post
